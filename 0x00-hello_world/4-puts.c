649926120beb2aa6ec6eaac1dfa6cf70bbfc324b
plusted <tddmaina@gmail.com>
2022-09-08 12:46:57 -0700

    Write a C program that prints asentence

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - writes out a string to the stdout
+ *
+ * Description: 'to display a string using the puts method
+ * Return: string value of stng
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
