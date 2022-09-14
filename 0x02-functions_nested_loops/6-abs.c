d011113b5c378962d02cda511c903bdb144b6a9e
plusted <tddmaina@gmail.com>
2022-09-14 13:41:17 -0700

    6-abs.c returns an absolute value of a number and main.h has been modified to include a prototype

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of a number
+ *
+ * Return: n*-1 for negative number or n otherwise
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
