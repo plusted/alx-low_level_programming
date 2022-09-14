f5f3c894816bd8edb9ddeaa32839a85c61eed71a
plusted <tddmaina@gmail.com>
2022-09-14 06:14:14 -0700

    prints the alphabet 10 times as well as modifications to the main.h file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: 0 success
+ */
+void print_alphabet_x10(void)
+{
+	int i, n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
