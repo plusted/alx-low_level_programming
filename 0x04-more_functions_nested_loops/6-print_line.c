e16a2af2a92e10a136db8e9ec049e75ad573231e
plusted <tddmaina@gmail.com>
2022-09-15 15:09:40 -0700

    6-print_line.c prints an _ n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - prints the _ character n number of times
+ * @n: number of times the line should be printed
+ *
+ * Return: 0 success
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n);
+	}
+	else
+	{
+		for (i = 1; i <=n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
