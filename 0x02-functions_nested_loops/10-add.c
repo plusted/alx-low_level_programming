4c52db3d43779cd29b04ce26b0e2cf70225c55df
plusted <tddmaina@gmail.com>
2022-09-15 04:10:17 -0700

    modified main.h to include a new prototype and add a 10-add.c file that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds to integers
+ * @a: first integer to be added
+ * @b: seccond integer to be added
+ *
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
