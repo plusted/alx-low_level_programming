cde649d56c51c6afa9db0faeac153809f4180caf
plusted <tddmaina@gmail.com>
2022-09-15 14:13:40 -0700

    3-print_numbers.c prints 0-9 and new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.c"
+/**
+ * print_numbers - prints numbers from 0-9
+ *
+ * Return: 0-9 \n
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
