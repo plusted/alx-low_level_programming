1399a9f73946a0fc74e2e2778c7dc876cc45b071
plusted <tddmaina@gmail.com>
2022-09-20 01:14:06 -0700

    added a 1-swap.c file that swaps the values of two ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first int to be swapped
+ * @b: second int to be swapped
+ *
+ * Return: 0 success
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
