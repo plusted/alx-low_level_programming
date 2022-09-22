96b4026460b166a88d1221bc0d18e8a4ee935416
plusted <tddmaina@gmail.com>
2022-09-22 02:56:08 -0700

    adds a file that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: new string will be added to dest
+ * @src: string to be added to dest
+ * @n: number of n bytes from src
+ *
+ * Return: the concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		if (src[j] == '\0')
+		break;
+
+	}
+	return(dest);
+}
