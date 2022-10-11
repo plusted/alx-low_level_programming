144c7cd210d095eb2c1a27baa3a768e3bd4066ba
plusted <tddmaina@gmail.com>
2022-10-11 03:20:19 -0700

    task 0, struct dog with 3 elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog - initalizes struct dog
+ * @name: name of the dog in struct
+ * @age: age of the dog in struct
+ * @owner: owner of the dog in struct
+ *
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H */
