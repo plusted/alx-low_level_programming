cb2df3f5d1f5de42efbe66b37b479f21df7178ce
plusted <tddmaina@gmail.com>
2022-10-11 03:42:42 -0700

    updated a prototype in the dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,17 @@ struct dog
 	char *owner;
 };
 
+int _putchar(char c);
+
+/**
+ * init_dog - a fucntion that initializes a variable of type struct
+ * @d: name of struct
+ * @name: name of the dog in struct
+ * @age: age of the dog in struct
+ * @owner: owner of the dog in struct
+ *
+ * Return: 0 successful always.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* _DOG_H */
